f2035a549798f89c6019a381b7fd1360ca03f7b8
Peter Ansell <p_ansell@yahoo.com>
2016-11-07 16:22:14 +1100

    More stub code
    Signed-off-by: Peter Ansell <p_ansell@yahoo.com>

--- src/main/java/com/github/ansell/cassandra/MaxRowSizeFinder.java
+++ src/main/java/com/github/ansell/cassandra/MaxRowSizeFinder.java
@@ -1,5 +1,10 @@
 package com.github.ansell.cassandra;
 
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.ResultSet;
+import com.datastax.driver.core.Row;
+import com.datastax.driver.core.Session;
+
 import joptsimple.OptionException;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
@@ -38,6 +43,15 @@ public final class MaxRowSizeFinder {
 		if (options.has(help)) {
 			parser.printHelpOn(System.out);
 			return;
+		}
+
+		try (Cluster cluster = Cluster.builder() // (1)
+				.addContactPoint("127.0.0.1").build();) {
+			Session session = cluster.connect(); // (2)
+
+			ResultSet rs = session.execute("select release_version from system.local"); // (3)
+			Row row = rs.one();
+			System.out.println(row.getString("release_version")); // (4)
 		}
 
 	}
